3dec22861e4bffa5ab5276bc12028e5369859da8
wohunlfry <1844157977@qq.com>
2022-08-10 01:35:14 +0800

    输入xml文件流，提取所需数据到实体

--- demo/src/main/java/com/example/demo/word/utils/ReadXmlSectionUtil.java
+++ demo/src/main/java/com/example/demo/word/utils/ReadXmlSectionUtil.java
@@ -1,9 +1,25 @@
 package com.example.demo.word.utils;
 
+import com.example.demo.word.model.SectionParam;
+
+import java.io.InputStream;
+
 /**
  * @author 曳戈泰尔
  * @version 1.0
  * @description 读取word章节内容
  * @date 2022/8/10 12:17 AM
  */
-public class ReadXmlSectionUtil {}
+public class ReadXmlSectionUtil {
+
+    /**
+     * 输入xml文件流，提取所需数据到实体
+     *
+     * @param in
+     * @return
+     */
+    public static SectionParam ReadXmlSection(InputStream in) {
+
+        return null;
+    }
+}
