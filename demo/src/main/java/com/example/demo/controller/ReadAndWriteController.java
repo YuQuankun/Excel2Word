9d9f5e69eaaf0862946cc5c106f0e83539662a8f
wohunlfry <1844157977@qq.com>
2022-08-14 18:45:04 +0800

    控制层增加通过xml下载文件 通过word下载文件

--- demo/src/main/java/com/example/demo/controller/ReadAndWriteController.java
+++ demo/src/main/java/com/example/demo/controller/ReadAndWriteController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.excel.model.ExcelData;
 import com.example.demo.excel.service.ReadExcelService;
 import com.example.demo.word.service.WriteWordService;
+import com.example.demo.word.utils.Word2XmlUtil;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import org.apache.commons.io.FileUtils;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -62,7 +64,53 @@ public class ReadAndWriteController {
         return writeWordService.writeToWord(excelData, newWordFile, wordFilePath);
     }
 
-    @PostMapping("/download")
+    @PostMapping("/download/xml")
+    @ResponseBody
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+                name = "excelFile",
+                value = "excelFile",
+                required = true,
+                dataType = "MultipartFile",
+                allowMultiple = true,
+                paramType = "form"),
+        @ApiImplicitParam(
+                name = "xmlFile",
+                value = "xmlFile",
+                required = true,
+                dataType = "MultipartFile",
+                allowMultiple = true,
+                paramType = "form")
+    })
+    public void readAndWriteAndDownloadByXML(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestPart("excelFile") MultipartFile excelFile,
+            @RequestPart("xmlFile") MultipartFile xmlFile)
+            throws IOException {
+
+        String excelSuffix =
+                Objects.requireNonNull(excelFile.getOriginalFilename())
+                        .substring(excelFile.getOriginalFilename().lastIndexOf(".") + 1);
+        String newExcelFileName = "./temp/" + UUID.randomUUID() + "." + excelSuffix;
+        File newExcelFile = new File((File) null, newExcelFileName);
+        FileUtils.copyInputStreamToFile(excelFile.getInputStream(), newExcelFile);
+        ExcelData excelData = excelService.getExcelData(new FileInputStream(newExcelFile));
+
+        String xmlSuffix =
+                Objects.requireNonNull(excelFile.getOriginalFilename())
+                        .substring(excelFile.getOriginalFilename().lastIndexOf(".") + 1);
+        String newXmlFileName = "./temp/" + UUID.randomUUID() + "." + xmlSuffix;
+        File newXmlFile = new File((File) null, newXmlFileName);
+        FileUtils.copyInputStreamToFile(xmlFile.getInputStream(), newXmlFile);
+
+        String wordFilePath = "./temp/" + UUID.randomUUID() + ".doc";
+        writeWordService.writeToWord(excelData, newXmlFile, wordFilePath);
+
+        writeWordService.downloadFile(request, response, wordFilePath);
+    }
+
+    @PostMapping("/download/word")
     @ResponseBody
     @ApiImplicitParams({
         @ApiImplicitParam(
@@ -80,22 +128,35 @@ public class ReadAndWriteController {
                 allowMultiple = true,
                 paramType = "form")
     })
-    public void readAndWriteAndDownload(
+    public void readAndWriteAndDownloadByWord(
             HttpServletRequest request,
             HttpServletResponse response,
             @RequestPart("excelFile") MultipartFile excelFile,
             @RequestPart("wordFile") MultipartFile wordFile)
             throws IOException {
 
-        File newExcelFile = new File((File) null, "demo/src/main/resource/tempExcel");
+        String excelSuffix =
+                Objects.requireNonNull(excelFile.getOriginalFilename())
+                        .substring(excelFile.getOriginalFilename().lastIndexOf(".") + 1);
+        String newExcelFileName = "./temp/" + UUID.randomUUID() + "." + excelSuffix;
+        File newExcelFile = new File((File) null, newExcelFileName);
         FileUtils.copyInputStreamToFile(excelFile.getInputStream(), newExcelFile);
         ExcelData excelData = excelService.getExcelData(new FileInputStream(newExcelFile));
 
-        File newWordFile = new File((File) null, "demo/src/main/resource/tempWord");
+        String wordSuffix =
+                Objects.requireNonNull(excelFile.getOriginalFilename())
+                        .substring(excelFile.getOriginalFilename().lastIndexOf(".") + 1);
+        String newWordFileName = "./temp/" + UUID.randomUUID() + "." + wordSuffix;
+        File newWordFile = new File((File) null, newWordFileName);
         FileUtils.copyInputStreamToFile(wordFile.getInputStream(), newWordFile);
 
-        String wordFilePath = "demo/src/main/resource/output/" + UUID.randomUUID() + ".doc";
-        writeWordService.writeToWord(excelData, newWordFile, wordFilePath);
+        // word转xml
+        String newXmlFileName = Word2XmlUtil.wordToXml(new FileInputStream(newWordFile));
+        assert newXmlFileName != null;
+        File newXmlFile = new File(newXmlFileName);
+
+        String wordFilePath = "./temp/" + UUID.randomUUID() + ".doc";
+        writeWordService.writeToWord(excelData, newXmlFile, wordFilePath);
 
         writeWordService.downloadFile(request, response, wordFilePath);
     }
