fd88912896c8ff114897d9176e0d6051064b03e7
wohunlfry <1844157977@qq.com>
2022-08-14 23:57:52 +0800

    份额参数分割bug修复

--- demo/src/main/java/com/example/demo/word/utils/ReadXmlParamUtil.java
+++ demo/src/main/java/com/example/demo/word/utils/ReadXmlParamUtil.java
@@ -7,16 +7,7 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.events.Characters;
-import javax.xml.stream.events.EndElement;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -31,7 +22,6 @@ import java.util.regex.Pattern;
 @Slf4j
 public class ReadXmlParamUtil {
 
-
     private static final Logger LOGGER = LoggerFactory.getLogger(ReadXmlParamUtil.class);
     /**
      * 输入xml文件路径，提取所需数据到实体
@@ -39,7 +29,7 @@ public class ReadXmlParamUtil {
      * @param fileUrlAndName
      * @return
      */
-    public static SectionParam readXmlParam(String fileUrlAndName)  {
+    public static SectionParam readXmlParam(String fileUrlAndName) {
 
         // 初始化返回实体
         SectionParam sectionParam = new SectionParam();
@@ -51,38 +41,41 @@ public class ReadXmlParamUtil {
 
         try {
 
-            //第一次正则匹配,匹配出第八部分的所有XML数据
-            String xmlContent = FileUtils.readFileToString(new File((File) null, fileUrlAndName), "utf-8");
-            Pattern pattern = Pattern.compile(bookMark.getBookMark2Deal() + ".*?" + bookMark.getBookMark3Deal());
+            // 第一次正则匹配,匹配出第八部分的所有XML数据
+            String xmlContent =
+                    FileUtils.readFileToString(new File((File) null, fileUrlAndName), "utf-8");
+            Pattern pattern =
+                    Pattern.compile(
+                            bookMark.getBookMark2Deal() + ".*?" + bookMark.getBookMark3Deal());
             Matcher matcher = pattern.matcher(xmlContent);
             String xmlContent8 = "";
-            if(matcher.find()){
+            if (matcher.find()) {
                 xmlContent8 = matcher.group();
             }
             System.out.println(xmlContent8);
 
-            //第二次正则,匹配出所有带百分号的数据
+            // 第二次正则,匹配出所有带百分号的数据
             Pattern pattern1 = Pattern.compile("[0-9]+%");
             Matcher matcher1 = pattern1.matcher(xmlContent8);
             int matcher_start = 0;
-            while (matcher1.find(matcher_start)){
-                quotaList.add(Float.parseFloat(matcher1.group(0).replace("%",""))/100);
+            while (matcher1.find(matcher_start)) {
+                quotaList.add(Float.parseFloat(matcher1.group(0).replace("%", "")) / 100);
                 matcher_start = matcher1.end();
             }
             sectionParam.setQuotaList(quotaList);
-        }
-        catch (Exception e){
-            LOGGER.error("获取表格参数失败:{}",e.getMessage());
+        } catch (Exception e) {
+            LOGGER.error("获取表格参数失败:{}", e.getMessage());
             e.printStackTrace();
         }
         return sectionParam;
     }
-    public static SectionParam readXmlParam(File wordFile)  {
+
+    public static SectionParam readXmlParam(File wordFile) {
 
         // 初始化返回实体
         SectionParam sectionParam = new SectionParam();
         // 初始化分割字符串
-        //TODO 增加方法
+        // TODO 增加方法
         BookMark bookMark = ReadXmlSectionUtil.setMark(wordFile);
 
         // 初始化参数数组
@@ -90,28 +83,39 @@ public class ReadXmlParamUtil {
 
         try {
 
-            //第一次正则匹配,匹配出第八部分的所有XML数据
+            // 第一次正则匹配,匹配出第八部分的所有XML数据
             String xmlContent = FileUtils.readFileToString(wordFile, "utf-8");
-            Pattern pattern = Pattern.compile(bookMark.getBookMark2Deal() + ".*?" + bookMark.getBookMark3Deal());
+            Pattern pattern =
+                    Pattern.compile(
+                            bookMark.getBookMark2Deal() + ".*?" + bookMark.getBookMark3Deal());
             Matcher matcher = pattern.matcher(xmlContent);
             String xmlContent8 = "";
-            if(matcher.find()){
+            if (matcher.find()) {
                 xmlContent8 = matcher.group();
             }
             System.out.println(xmlContent8);
 
-            //第二次正则,匹配出所有带百分号的数据
-            Pattern pattern1 = Pattern.compile("[0-9]+%");
+            // 第二次正则,匹配出所有带百分号的数据
+            Pattern pattern1 = Pattern.compile("<w:t>[0-9]+.*?</w:t>");
             Matcher matcher1 = pattern1.matcher(xmlContent8);
             int matcher_start = 0;
-            while (matcher1.find(matcher_start)){
-                quotaList.add(Float.parseFloat(matcher1.group(0).replace("%",""))/100);
+            String temp = "";
+            while (matcher1.find(matcher_start)) {
+
+                // 针对拆分拼接
+                temp = temp + matcher1.group(0);
+                if (temp.contains("%")) {
+                    temp = temp.replace("<w:t>", "").replace("</w:t>", "");
+                    quotaList.add(Float.parseFloat(temp.replace("%", "")) / 100);
+
+                    temp = "";
+                }
+
                 matcher_start = matcher1.end();
             }
             sectionParam.setQuotaList(quotaList);
-        }
-        catch (Exception e){
-            LOGGER.error("获取表格参数失败:{}",e.getMessage());
+        } catch (Exception e) {
+            LOGGER.error("获取表格参数失败:{}", e.getMessage());
             e.printStackTrace();
         }
         return sectionParam;
